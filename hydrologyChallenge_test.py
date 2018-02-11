ccb369cb87c0c4aaa846c5cd231e2d4463c067dc
steve-ng562 <31748605+steve-ng562@users.noreply.github.com>
2018-02-10 16:53:40 -0800

    Update hydrologyChallenge_test.py

--- hydrologyChallenge_test.py
+++ hydrologyChallenge_test.py
@@ -4,8 +4,8 @@ import hydrologyChallenge
 def test():  # compare test data to validation data
     data = hydrologyChallenge.process("http://rapid-hub.org/data/angles_UCI_CS.csv")
     data2 = hydrologyChallenge.process("http://rapid-hub.org/data/angles_UCI_CS_out.csv")
-    assert data.loc[:, "angle_degrees"].tolist() == data.loc2[:, "angle_degrees"].tolist()
-    assert data.loc[:, "station_id"].tolist() == data.loc2[:, "station_id"].tolist()
+    assert data2.loc[:, "angle_degrees"].tolist() == data.loc[:, "angle_degrees"].tolist()
+    assert data2.loc[:, "station_id"].tolist() == data.loc[:, "station_id"].tolist()
 
 
 
